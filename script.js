a83cae7d4945f19b1110b8943d09f64dd378b1c0
lucasaleixo <lap15.ufpr@gmail.com>
2021-10-08 18:52:19 -0300

    implemented loans

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function(mov, i) {
+  const movs = sort ? movements.slice().sort((a,b) => a - b) : movements;
+
+  movs.forEach(function(mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -158,7 +160,7 @@ btnTransfer.addEventListener('click', function(e) {
     acc => acc.username === inputTransferTo.value
   ); 
 
-  inputTransferAmount.value = inputTransferTo.value ='';
+  inputTransferAmount.value = inputTransferTo.value = '';
 
   if (amount > 0 
     &&recieverAcc
@@ -169,4 +171,43 @@ btnTransfer.addEventListener('click', function(e) {
 
       updateUI(currentAccount);
   }
+});
+
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  } 
+
+  inputLoanAmount.value = '';
+}); 
+
+btnClose.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if(inputCloseUsername.value === currentAccount.username
+    && Number(inputClosePin.value) ===  currentAccount.pin) {
+      const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+
+      // Delete account
+      accounts.splice(index, 1);
+
+      // Hide UI
+      containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
